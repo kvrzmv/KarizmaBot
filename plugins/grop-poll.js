6d2a141e66519b463fc2e86125e72bfdd559b4b3
kvrzmv <165571581+kvrzmv@users.noreply.github.com>
2024-07-04 14:34:36 +0300

    🕸 𝙸𝚃𝙰𝙲𝙷𝚈 𝙱𝙾𝚃🕷
    تم إنشاء البوت بواسطه كاريزما ويتم حفظ جميع الحقوق اليه

---
+++ plugins/grop-poll.js
@@ -0,0 +1,29 @@
+//تم تطوير بواسطه كاريزما 
+//استمتع بالبوت ي حبي
+//المصدر
+//https://t.me/kvrzm
+
+const handler = async (m, {conn, text, args, usedPrefix, command}) => {
+  let name = await conn.getName(m.sender);
+  if (name == 'undefined') name = 'Indefinido';
+  const b = text.split('|');
+  if (!b[1]) throw `*⚠️ الامر بيشتغل كدا ${usedPrefix + command} الافضل؟|رونالدو|ميسي*`;
+  if (b[12]) throw `*⚠️ الامر بيشتغل كدا  ${usedPrefix + command} الافضل؟|رونالدو|ميسي*`;
+  const caption = `*مقدم التصويت:*\n${name}\n*السؤال ↧↧:*\n${text.split('|')[0]}`.trim();
+  const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
+  const sendPollMessage = {
+    messageContextInfo: {
+        messageSecret: "bT3tfZngfSMWK2zOEL8pSclPG+xldidYDX+ybB8vdEw="
+    },
+    pollCreationMessage: {
+        name: caption,
+        options: options,
+        selectableOptionsCount: 1,
+    }
+  };
+conn.relayMessage(m.chat, sendPollMessage, {quoted: m});
+};
+handler.help = ['encuesta question|option|option'];
+handler.tags = ['group'];
+handler.command = ['poll','pooll','تصويت'];
+export default handler;
