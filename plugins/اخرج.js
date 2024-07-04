67d7116215b1e32bb5254181a437604afe80a626
kvrzmv <165571581+kvrzmv@users.noreply.github.com>
2024-07-04 16:48:42 +0300

    🕸 𝙸𝚃𝙰𝙲𝙷𝚈 𝙱𝙾𝚃🕷
    //تم تطوير بواسطه كاريزما 
    //استمتع بالبوت ي حبي
    //المصدر
    //https://t.me/kvrzm

--- plugins/اخرج.js
+++ plugins/اخرج.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*لقد قضيت معكم وقتاً رائعاً الى اللقاء تحياتي ميجو بوت ⁦^⁠_⁠^⁩*') 
+await conn.reply(id, '*كسمكم كلكم انا طالع  ⁦^⁠_⁠^⁩*') 
 await conn.groupLeave(id)}
 handler.command = /^(اخرج|اطلع|غادر|خروج)$/i
 handler.group = true
